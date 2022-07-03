0ec8ed207eca44e206612685fd935bd94f21c36b
pROmeTEuL <radu.teodor.vatra@gmail.com>
2022-07-03 20:42:54 +0300

    thomas

--- draw.cpp
+++ draw.cpp
@@ -7,12 +7,20 @@ void Engine::draw()
         m_Window.setView(m_BGMainView);
         m_Window.draw(m_BackgroundSprite);
         m_Window.setView(m_MainView);
+        m_Window.draw(m_Thomas.getSprite());
+        m_Window.draw(m_Bob.getSprite());
     } else {
         m_Window.setView(m_BGLeftView);
         m_Window.draw(m_BackgroundSprite);
+        m_Window.setView(m_LeftView);
+        m_Window.draw(m_Bob.getSprite());
+        m_Window.draw(m_Thomas.getSprite());
+        ///////////////////////////////////////////////
         m_Window.setView(m_BGRightView);
         m_Window.draw(m_BackgroundSprite);
         m_Window.setView(m_RightView);
+        m_Window.draw(m_Thomas.getSprite());
+        m_Window.draw(m_Bob.getSprite());
     }
     m_Window.setView(m_HudView);
     m_Window.display();
--- update.cpp
+++ update.cpp
@@ -3,9 +3,26 @@
 
 void Engine::update(float dtAsSeconds)
 {
+    if (m_NewLevelRequired) {
+        m_Thomas.spawn(Vector2f(0, 0), GRAVITY);
+        m_Bob.spawn(Vector2f(0, 0), GRAVITY);
+        m_TimeRemaining = 10;
+        m_NewLevelRequired = false;
+    }
     if (m_Playing) {
+        m_Thomas.update(dtAsSeconds);
+        m_Bob.update(dtAsSeconds);
         m_TimeRemaining -= dtAsSeconds;
         if (m_TimeRemaining <= 0)
             m_NewLevelRequired = true;
+    }
+    if(m_SplitScreen) {
+        m_LeftView.setCenter(m_Thomas.getCenter());
+        m_RightView.setCenter(m_Bob.getCenter());
+    } else {
+        if (m_Character1)
+            m_MainView.setCenter(m_Thomas.getCenter());
+        else
+            m_MainView.setCenter(m_Bob.getCenter());
     }
 }
